d363037780027d03bcfdc2194b7cb1a9335724a1
Eric Crane <eric.crane@mac.com>
2018-12-28 16:06:00 -0500

    adds run script

--- lib/outline_script.rb
+++ lib/outline_script.rb
@@ -15,16 +15,3 @@ module OutlineScript
   end
   
 end
-
-
-#
-# The following can be used to run the application in CLI mode
-# in development and use source rather than the gem.
-# 
-# From the /lib/ directory:  ruby outline_script.rb
-# 
-path = File.dirname( File.absolute_path( __FILE__ ) )
-root = File.join( path, "outline_script", "**/*.rb" )
-Dir.glob( root ) { |ruby_file| require ruby_file }
-
-OutlineScript.run
---
+++ lib/run.rb
@@ -0,0 +1,13 @@
+require 'outline_script'
+
+#
+# The following can be used to run the application in CLI mode
+# in development and use source rather than the gem.
+# 
+# From the /lib/ directory:  ruby run.rb
+# 
+path = File.dirname( File.absolute_path( __FILE__ ) )
+root = File.join( path, "outline_script", "**/*.rb" )
+Dir.glob( root ) { |ruby_file| require ruby_file }
+
+OutlineScript.run
