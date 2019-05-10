34de26a98819b6144b46965f179f921af0155441
Eric Crane <eric.crane@mac.com>
2019-05-10 15:39:34 -0400

    adds slack object

---
+++ lib/gloo/objs/slack.rb
@@ -0,0 +1,109 @@
+# Author::    Eric Crane  (mailto:eric.crane@mac.com)
+# Copyright:: Copyright (c) 2019 Eric Crane.  All rights reserved.
+#
+# An object that can send a message to a slack channel.
+#
+require 'net/http'
+require 'uri'
+require 'json'
+
+module Gloo
+  module Objs
+    class Slack < Gloo::Core::Obj
+      
+      KEYWORD = 'slack'
+      KEYWORD_SHORT = 'slack'
+      URL = 'uri'
+      MSG = 'message'
+      USER = 'username'
+      CHANNEL = 'channel'
+      ICON = 'icon_emoji'
+
+      # 
+      # The name of the object type.
+      # 
+      def self.typename
+        return KEYWORD
+      end
+
+      # 
+      # The short name of the object type.
+      # 
+      def self.short_typename
+        return KEYWORD_SHORT
+      end
+      
+      # 
+      # Get the URI from the child object.
+      # Returns nil if there is none.
+      # 
+      def get_uri
+        uri = find_child URL
+        return nil unless uri
+        return uri.value
+      end
+      
+      # 
+      # Get all the children of the body container and 
+      # convert to JSON that will be sent in the HTTP body.
+      # 
+      def get_body_as_json
+        h = { 'text' => find_child( MSG ).value,
+          'username' => find_child( USER ).value,
+          'channel' => find_child( CHANNEL ).value,
+          'icon_emoji' => find_child( ICON ).value,
+         }
+        
+        return h.to_json
+      end
+
+
+      # ---------------------------------------------------------------------
+      #    Children
+      # ---------------------------------------------------------------------
+
+      # Does this object have children to add when an object
+      # is created in interactive mode?
+      # This does not apply during obj load, etc.
+      def add_children_on_create?
+        return true
+      end
+      
+      # Add children to this object.
+      # This is used by containers to add children needed 
+      # for default configurations.
+      def add_default_children
+        fac = $engine.factory
+        fac.create URL, "string", "https://hooks.slack.com/services/...", self
+        fac.create MSG, "string", "textual message", self
+        fac.create USER, "string", "Slack Bot", self
+        fac.create CHANNEL, "string", "general", self
+        fac.create ICON, "string", ":ghost:", self
+      end
+
+      
+      # ---------------------------------------------------------------------
+      #    Messages
+      # ---------------------------------------------------------------------
+
+      # 
+      # Get a list of message names that this object receives.
+      # 
+      def self.messages
+        return super + [ "run" ]
+      end
+      
+      # Post the content to the endpoint.
+      def msg_run
+        uri = get_uri
+        return unless uri
+        
+        body = get_body_as_json
+        puts body
+        
+        Gloo::Objs::HttpPost.post_json uri, body, true
+      end
+      
+    end
+  end
+end
---
+++ test/objs/slack_test.rb
@@ -0,0 +1,49 @@
+require "test_helper"
+
+class SlackTest < Minitest::Test
+  
+  def setup
+    @engine = Gloo::App::Engine.new( [ "--quiet" ] )
+    @engine.start
+    @dic = @engine.dictionary
+  end
+
+  def test_the_typename
+    assert_equal "slack", Gloo::Objs::Slack.typename
+  end
+
+  def test_the_short_typename
+    assert_equal "slack", Gloo::Objs::Slack.short_typename
+  end
+
+  def test_find_type
+    assert @dic.find_obj( "slack" )
+    assert @dic.find_obj( "SLACK" )
+  end
+
+  def test_messages
+    msgs = Gloo::Objs::Slack.messages
+    assert msgs
+    assert msgs.include?( "run" )
+    assert msgs.include?( "unload" )
+  end
+
+  def test_adds_children_on_create
+    o = Gloo::Objs::Slack.new
+    assert o.add_children_on_create?
+  end
+
+  def test_that_children_are_added_on_create
+    i = @engine.parser.parse_immediate 'create s as slack'
+    i.run
+    assert_equal 1, @engine.heap.root.child_count
+    obj = @engine.heap.root.children.first
+    assert obj
+    assert_equal "s", obj.name
+    assert_equal 5, obj.child_count
+    assert_equal "uri", obj.children.first.name
+    assert_equal "message", obj.children[1].name
+    assert_equal "icon_emoji", obj.children.last.name
+  end
+
+end
