493eb45c09b17133a7bd1e598a72ceeed4e8d1f7
Eric Crane <eric.crane@mac.com>
2019-05-20 16:34:09 -0400

    adds confirm prompt

---
+++ lib/gloo/objs/cli/confirm.rb
@@ -0,0 +1,92 @@
+# Author::    Eric Crane  (mailto:eric.crane@mac.com)
+# Copyright:: Copyright (c) 2019 Eric Crane.  All rights reserved.
+#
+# Show a CLI confirmation prompt.
+#
+
+module Gloo
+  module Objs
+    class Confirm < Gloo::Core::Obj
+      
+      KEYWORD = 'confirm'
+      KEYWORD_SHORT = 'confirm'
+      PROMPT = 'prompt'
+      RESULT = 'result'
+
+      # 
+      # The name of the object type.
+      # 
+      def self.typename
+        return KEYWORD
+      end
+
+      # 
+      # The short name of the object type.
+      # 
+      def self.short_typename
+        return KEYWORD_SHORT
+      end
+      
+      # 
+      # Get the URI from the child object.
+      # Returns nil if there is none.
+      # 
+      def get_prompt
+        o = find_child PROMPT
+        return nil unless o
+        return o.value
+      end
+      
+      # 
+      # Set the result of the system call.
+      # 
+      def set_result data
+        r = find_child RESULT
+        return nil unless r
+        r.set_value data
+      end
+            
+
+      # ---------------------------------------------------------------------
+      #    Children
+      # ---------------------------------------------------------------------
+
+      # Does this object have children to add when an object
+      # is created in interactive mode?
+      # This does not apply during obj load, etc.
+      def add_children_on_create?
+        return true
+      end
+      
+      # Add children to this object.
+      # This is used by containers to add children needed 
+      # for default configurations.
+      def add_default_children
+        fac = $engine.factory
+        fac.create "prompt", "string", "> ", self
+        fac.create "result", "boolean", nil, self
+      end
+
+      
+      # ---------------------------------------------------------------------
+      #    Messages
+      # ---------------------------------------------------------------------
+
+      # 
+      # Get a list of message names that this object receives.
+      # 
+      def self.messages
+        return super + [ "run" ]
+      end
+      
+      # Run the system command.
+      def msg_run
+        prompt = get_prompt
+        return unless prompt
+        result = $prompt.yes?( prompt )
+        set_result result
+      end
+      
+    end
+  end
+end
---
+++ test/objs/cli/confirm_test.rb
@@ -0,0 +1,48 @@
+require "test_helper"
+
+class ConfirmTest < Minitest::Test
+  
+  def setup
+    @engine = Gloo::App::Engine.new( [ "--quiet" ] )
+    @engine.start
+    @dic = @engine.dictionary
+  end
+
+  def test_the_typename
+    assert_equal "confirm", Gloo::Objs::Confirm.typename
+  end
+
+  def test_the_short_typename
+    assert_equal "confirm", Gloo::Objs::Confirm.short_typename
+  end
+
+  def test_find_type
+    assert @dic.find_obj( "prompt" )
+    assert @dic.find_obj( "ask" )
+  end
+
+  def test_messages
+    msgs = Gloo::Objs::Confirm.messages
+    assert msgs
+    assert msgs.include?( "run" )
+    assert msgs.include?( "unload" )
+  end
+
+  def test_adds_children_on_create
+    o = Gloo::Objs::Confirm.new
+    assert o.add_children_on_create?
+  end
+
+  def test_that_children_are_added_on_create
+    i = @engine.parser.parse_immediate 'create o as confirm'
+    i.run
+    assert_equal 1, @engine.heap.root.child_count
+    obj = @engine.heap.root.children.first
+    assert obj
+    assert_equal "o", obj.name
+    assert_equal 2, obj.child_count
+    assert_equal "prompt", obj.children.first.name
+    assert_equal "result", obj.children.last.name
+  end
+
+end
