13488fedcf459c692776ddcf78e978dec42d1099
Eric Crane <eric.crane@mac.com>
2019-05-30 16:15:02 -0400

    adds teams webhook integration

---
+++ test/objs/web/teams_test.rb
@@ -0,0 +1,50 @@
+require "test_helper"
+
+class TeamsTest < Minitest::Test
+  
+  def setup
+    @engine = Gloo::App::Engine.new( [ "--quiet" ] )
+    @engine.start
+    @dic = @engine.dictionary
+  end
+
+  def test_the_typename
+    assert_equal "teams", Gloo::Objs::Teams.typename
+  end
+
+  def test_the_short_typename
+    assert_equal "team", Gloo::Objs::Teams.short_typename
+  end
+
+  def test_find_type
+    assert @dic.find_obj( "teams" )
+    assert @dic.find_obj( "Team" )
+  end
+
+  def test_messages
+    msgs = Gloo::Objs::Teams.messages
+    assert msgs
+    assert msgs.include?( "run" )
+    assert msgs.include?( "unload" )
+  end
+
+  def test_adds_children_on_create
+    o = Gloo::Objs::Teams.new
+    assert o.add_children_on_create?
+  end
+
+  def test_that_children_are_added_on_create
+    i = @engine.parser.parse_immediate 'create t as teams'
+    i.run
+    assert_equal 1, @engine.heap.root.child_count
+    obj = @engine.heap.root.children.first
+    assert obj
+    assert_equal "t", obj.name
+    assert_equal 4, obj.child_count
+    assert_equal "uri", obj.children.first.name
+    assert_equal "title", obj.children[1].name
+		assert_equal "color", obj.children[2].name
+    assert_equal "message", obj.children.last.name
+  end
+
+end
