5e3143191aefadd929189208855e84368b82710a
Eric Crane <eric.crane@mac.com>
2020-08-12 11:30:35 -0400

    fix bug with message dispatch

--- lib/gloo/verbs/tell.rb
+++ lib/gloo/verbs/tell.rb
@@ -22,7 +22,7 @@ module Gloo
         o = pn.resolve
 
         if o
-          Gloo::Exec::Dispatch.message 'run', o, @params
+          Gloo::Exec::Dispatch.message msg, o, @params
         else
           $log.error "Could not send message to object.  Bad path: #{name}"
         end
