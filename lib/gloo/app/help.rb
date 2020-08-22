04b9419916f4773adadc4b3f6d4f8584c9941af4
Eric Crane <eric.crane@mac.com>
2020-08-22 19:08:48 -0400

    starting work on expanded help system

--- lib/gloo/app/help.rb
+++ lib/gloo/app/help.rb
@@ -4,6 +4,9 @@
 # Help system.
 #
 
+require 'tty-markdown'
+require 'tty-pager'
+
 module Gloo
   module App
     class Help
@@ -11,6 +14,8 @@ module Gloo
       # Get text to display when the application is run
       # in HELP mode.
       def self.get_help_text
+        load_help_files
+
         return <<~TEXT
           NAME
           \tgloo
@@ -28,6 +33,21 @@ module Gloo
           \t--help \t\t - Show this help page
 
         TEXT
+      end
+
+      def self.load_help_files
+        pn = File.dirname( File.absolute_path( __FILE__ ) )
+        pn = File.dirname( pn )
+        root = File.join( pn, 'help', '**/*.md' )
+        Dir.glob( root ).each do |md_file|
+          $log.debug md_file
+          data = File.read md_file
+
+          puts TTY::Markdown.parse data
+          # md = TTY::Markdown.parse data
+          # pager = TTY::Pager.new
+          # pager.page( md )
+        end
       end
 
     end
