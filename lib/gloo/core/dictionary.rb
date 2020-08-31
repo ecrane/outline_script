1d87f2290c19f741fe75af87ffc6f467b5d8b1e1
Eric Crane <eric.crane@mac.com>
2020-08-31 17:08:04 -0400

    clean up in dictionary

--- lib/gloo/core/dictionary.rb
+++ lib/gloo/core/dictionary.rb
@@ -13,7 +13,9 @@ module Gloo
 
       attr_reader :verbs, :objs, :keywords
 
+      #
       # Set up the object dictionary.
+      #
       def initialize
         @verbs = {}
         @objs = {}
@@ -22,55 +24,41 @@ module Gloo
         @keywords = []
       end
 
+      #
       # Register a verb.
+      #
       def register_verb( subclass )
         @verb_references << subclass
       end
 
+      #
       # Register an object type.
+      #
       def register_obj( subclass )
         @obj_references << subclass
       end
 
+      #
       # Initialize verbs and objects in the dictionary.
+      #
       def init
         $log.debug 'initializing dictionaries'
         init_verbs
         init_objs
       end
 
-      # Init the list of verbs.
-      def init_objs
-        $log.debug "initializing #{@obj_references.count} objects"
-        @obj_references.each do |o|
-          $log.debug o
-          @objs[ o.typename ] = o
-          @objs[ o.short_typename ] = o
-          self.add_key o.typename
-          self.add_key o.short_typename if o.typename != o.short_typename
-        end
-      end
-
       #
-      # Add a keyword to the keyword list.
-      # Report an error if the keyword is already in the list.
-      def add_key( keyword )
-        if @keywords.include?( keyword )
-          $log.error "duplicate keyword '#{keyword}'"
-          return
-        end
-
-        @keywords << keyword
-      end
-
       # Is the given word an object type?
+      #
       def obj?( word )
         return false unless word
 
         return @objs.key?( word.downcase )
       end
 
+      #
       # Find the object type by name.
+      #
       def find_obj( word )
         return nil unless word
         return nil unless obj?( word )
@@ -78,27 +66,18 @@ module Gloo
         return @objs[ word.downcase ]
       end
 
-      # Init the list of verbs.
-      def init_verbs
-        $log.debug "initializing #{@verb_references.count} verbs"
-        @verb_references.each do |v|
-          $log.debug v
-          @verbs[ v.keyword ] = v
-          @verbs[ v.keyword_shortcut ] = v
-          # v.send( :new ).run
-          self.add_key v.keyword
-          self.add_key v.keyword_shortcut if v.keyword != v.keyword_shortcut
-        end
-      end
-
+      #
       # Is the given word a verb?
+      #
       def verb?( word )
         return false unless word
 
         return @verbs.key?( word.downcase )
       end
 
+      #
       # Find the verb by name.
+      #
       def find_verb( verb )
         return nil unless verb
         return nil unless verb?( verb )
@@ -106,12 +85,16 @@ module Gloo
         return @verbs[ verb.downcase ]
       end
 
+      #
       # Get the list of verbs, sorted.
+      #
       def get_obj_types
         return @obj_references.sort { |a, b| a.typename <=> b.typename }
       end
 
+      #
       # Get the list of verbs, sorted.
+      #
       def get_verbs
         return @verb_references.sort { |a, b| a.keyword <=> b.keyword }
       end
@@ -142,6 +125,54 @@ module Gloo
             puts str
             str = ''
           end
+        end
+      end
+
+      # ---------------------------------------------------------------------
+      #    Private
+      # ---------------------------------------------------------------------
+
+      private
+
+      #
+      # Add a keyword to the keyword list.
+      # Report an error if the keyword is already in the list.
+      #
+      def add_key( keyword )
+        if @keywords.include?( keyword )
+          $log.error "duplicate keyword '#{keyword}'"
+          return
+        end
+
+        @keywords << keyword
+      end
+
+      #
+      # Init the list of objects.
+      #
+      def init_objs
+        $log.debug "initializing #{@obj_references.count} objects"
+        @obj_references.each do |o|
+          $log.debug o
+          @objs[ o.typename ] = o
+          @objs[ o.short_typename ] = o
+          add_key o.typename
+          add_key o.short_typename if o.typename != o.short_typename
+        end
+      end
+
+      #
+      # Init the list of verbs.
+      #
+      def init_verbs
+        $log.debug "initializing #{@verb_references.count} verbs"
+        @verb_references.each do |v|
+          $log.debug v
+          @verbs[ v.keyword ] = v
+          @verbs[ v.keyword_shortcut ] = v
+          # v.send( :new ).run
+          add_key v.keyword
+          add_key v.keyword_shortcut if v.keyword != v.keyword_shortcut
         end
       end
 
