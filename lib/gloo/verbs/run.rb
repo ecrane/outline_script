c9b822e69a91c610488261f096d966ecd76d83f9
Eric Crane <eric.crane@mac.com>
2020-09-23 14:51:48 -0400

    cleanup run verb

--- lib/gloo/verbs/run.rb
+++ lib/gloo/verbs/run.rb
@@ -12,33 +12,22 @@ module Gloo
       KEYWORD = 'run'.freeze
       KEYWORD_SHORT = 'r'.freeze
       EVALUATE_RUN = '~>'.freeze
+      MISSING_EXPR_ERR = 'Missing Expression!'.freeze
 
       #
       # Run the verb.
       #
       def run
+        if @tokens.token_count < 2
+          $engine.err MISSING_EXPR_ERR
+          return
+        end
+
         if @tokens.second == EVALUATE_RUN
           run_expression
         else
           run_script
         end
-      end
-
-      #
-      # Run a script specified by pathname
-      #
-      def run_script
-        Gloo::Exec::Runner.run @tokens.second
-      end
-
-      #
-      # Evaluate an expression and run that.
-      #
-      def run_expression
-        return unless @tokens.token_count > 2
-
-        expr = Gloo::Expr::Expression.new( @tokens.params[ 1..-1 ] )
-        $engine.parser.run expr.evaluate
       end
 
       #
@@ -53,6 +42,32 @@ module Gloo
       #
       def self.keyword_shortcut
         return KEYWORD_SHORT
+      end
+
+      # ---------------------------------------------------------------------
+      #    Private functions
+      # ---------------------------------------------------------------------
+
+      private
+
+      #
+      # Run a script specified by pathname
+      #
+      def run_script
+        Gloo::Exec::Runner.run @tokens.second
+      end
+
+      #
+      # Evaluate an expression and run that.
+      #
+      def run_expression
+        unless @tokens.token_count > 2
+          $engine.err MISSING_EXPR_ERR
+          return
+        end
+
+        expr = Gloo::Expr::Expression.new( @tokens.params[ 1..-1 ] )
+        $engine.parser.run expr.evaluate
       end
 
     end
--- test/verbs/run_test.rb
+++ test/verbs/run_test.rb
@@ -17,26 +17,34 @@ class RunTest < Minitest::Test
 
   def test_running_script
     s = 'create s as script : "show 3 + 4"'
-    i = @engine.parser.parse_immediate s
-    i.run
+    @engine.parser.run s
     assert_equal 1, @engine.heap.root.child_count
-    i = @engine.parser.parse_immediate 'run s'
-    i.run
+    @engine.parser.run 'run s'
     assert_equal 7, @engine.heap.it.value
   end
 
   def test_running_evaluated_string
     s = 'create s as string : "show 3 + 4"'
-    i = @engine.parser.parse_immediate s
-    i.run
+    @engine.parser.run s
     assert_equal 1, @engine.heap.root.child_count
-    i = @engine.parser.parse_immediate 'run ~> s'
-    i.run
+    @engine.parser.run 'run ~> s'
     assert_equal 7, @engine.heap.it.value
   end
 
   def test_help_text
     assert @engine.help.topic? Gloo::Verbs::Run.keyword
+  end
+
+  def test_without_expression
+    @engine.parser.run 'run'
+    assert @engine.error?
+    assert_equal Gloo::Verbs::Run::MISSING_EXPR_ERR, @engine.heap.error.value
+  end
+
+  def test_evaluated_without_expression
+    @engine.parser.run 'run ~>'
+    assert @engine.error?
+    assert_equal Gloo::Verbs::Run::MISSING_EXPR_ERR, @engine.heap.error.value
   end
 
 end
