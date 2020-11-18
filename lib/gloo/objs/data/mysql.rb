b6fdd34df0e6c0051d5246b7821fd1fa3b837863
Eric Crane <eric.crane@mac.com>
2020-11-18 16:51:12 -0500

    mysql obj first pass

---
+++ lib/gloo/objs/data/mysql.rb
@@ -0,0 +1,145 @@
+# Author::    Eric Crane  (mailto:eric.crane@mac.com)
+# Copyright:: Copyright (c) 2020 Eric Crane.  All rights reserved.
+#
+# A MySQL database connection.
+#
+#
+# https://github.com/brianmario/mysql2
+# https://www.rubydoc.info/gems/mysql2/0.2.3/Mysql2/Client
+#
+require 'mysql2'
+
+module Gloo
+  module Objs
+    class Mysql < Gloo::Core::Obj
+
+      KEYWORD = 'mysql'.freeze
+      KEYWORD_SHORT = 'mysql'.freeze
+
+      #
+      # user     = opts[:username]
+      #   pass     = opts[:password]
+      #   host     = opts[:host] || 'localhost'
+      #   port     = opts[:port] || 3306
+      #   database = opts[:database]
+      #   socket   = opts[:socket]
+      #   flags    = opts[:flags] || 0
+      #
+      HOST = 'host'.freeze
+      DB = 'database'.freeze
+      USER = 'username'.freeze
+      PASSWD = 'password'.freeze
+
+      NOT_IMPLEMENTED_ERR = 'Not implemented yet!'.freeze
+
+      #
+      # The name of the object type.
+      #
+      def self.typename
+        return KEYWORD
+      end
+
+      #
+      # The short name of the object type.
+      #
+      def self.short_typename
+        return KEYWORD_SHORT
+      end
+
+      # ---------------------------------------------------------------------
+      #    Children
+      # ---------------------------------------------------------------------
+
+      #
+      # Does this object have children to add when an object
+      # is created in interactive mode?
+      # This does not apply during obj load, etc.
+      #
+      def add_children_on_create?
+        return true
+      end
+
+      #
+      # Add children to this object.
+      # This is used by containers to add children needed
+      # for default configurations.
+      #
+      def add_default_children
+        fac = $engine.factory
+        fac.create_string HOST, nil, self
+        fac.create_string DB, nil, self
+        fac.create_string USER, nil, self
+        fac.create_string PASSWD, nil, self
+      end
+
+      # ---------------------------------------------------------------------
+      #    Messages
+      # ---------------------------------------------------------------------
+
+      #
+      # Get a list of message names that this object receives.
+      #
+      def self.messages
+        return super + [ 'verify' ]
+      end
+
+      #
+      # SSH to the host and execute the command, then update result.
+      #
+      def msg_verify
+        $engine.err NOT_IMPLEMENTED_ERR
+      end
+
+      # ---------------------------------------------------------------------
+      #    Private functions
+      # ---------------------------------------------------------------------
+
+      private
+
+      #
+      # Get the host from the child object.
+      # Returns nil if there is none.
+      #
+      def host_value
+        o = find_child HOST
+        return nil unless o
+
+        return o.value
+      end
+
+      #
+      # Get the Database name from the child object.
+      # Returns nil if there is none.
+      #
+      def db_value
+        o = find_child DB
+        return nil unless o
+
+        return o.value
+      end
+
+      #
+      # Get the Username from the child object.
+      # Returns nil if there is none.
+      #
+      def user_value
+        o = find_child USER
+        return nil unless o
+
+        return o.value
+      end
+
+      #
+      # Get the Password name from the child object.
+      # Returns nil if there is none.
+      #
+      def passwd_value
+        o = find_child PASSWD
+        return nil unless o
+
+        return o.value
+      end
+
+    end
+  end
+end
---
+++ test/objs/data/mysql_test.rb
@@ -0,0 +1,55 @@
+require 'test_helper'
+
+class MysqlTest < Minitest::Test
+
+  def setup
+    @engine = Gloo::App::Engine.new( [ '--quiet' ] )
+    @engine.start
+    @dic = @engine.dictionary
+  end
+
+  def test_the_typename
+    assert_equal 'mysql', Gloo::Objs::Mysql.typename
+  end
+
+  def test_the_short_typename
+    assert_equal 'mysql', Gloo::Objs::Mysql.short_typename
+  end
+
+  def test_find_type
+    assert @dic.find_obj( 'mysql' )
+    assert @dic.find_obj( 'MySQL' )
+    assert @dic.find_obj( 'Mysql' )
+  end
+
+  def test_messages
+    msgs = Gloo::Objs::Mysql.messages
+    assert msgs
+    assert msgs.include?( 'verify' )
+    assert msgs.include?( 'unload' )
+  end
+
+  def test_adds_children_on_create
+    o = Gloo::Objs::Mysql.new
+    assert o.add_children_on_create?
+  end
+
+  def test_that_children_are_added_on_create
+    i = @engine.parser.parse_immediate 'create o as mysql'
+    i.run
+    assert_equal 1, @engine.heap.root.child_count
+    obj = @engine.heap.root.children.first
+    assert obj
+    assert_equal 'o', obj.name
+    assert_equal 4, obj.child_count
+    assert_equal 'host', obj.children.first.name
+    assert_equal 'database', obj.children[ 1 ].name
+    assert_equal 'username', obj.children[ 2 ].name
+    assert_equal 'password', obj.children.last.name
+  end
+
+  def test_help_text
+    assert @engine.help.topic? Gloo::Objs::Mysql.typename
+  end
+
+end
